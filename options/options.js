7221a1fa32c33849b2af9138d1c6cbbf5405834a
Crowdbotics <team@crowdbotics.com>
2021-10-20 21:50:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "aged_water_31604",
+  url: "https://aged_water_31604.botics.co",
+  api: "https://aged_water_31604.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
